2fbd9015a61bac2d69b23b7432bb375bb87bf579
Jacksonhoch1 <jacksonhoch1@gmail.com>
2023-02-09 12:00:04 -0500

    bumped to wpilib 2023.3.2

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Drivetrain motors, as well as camera and odometry
+ */
 public class Drivetrain extends SubsystemBase{
     
 }
