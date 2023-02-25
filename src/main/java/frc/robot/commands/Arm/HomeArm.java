ba911f65f5552d804e1974cc77deea89c39a036d
Team3767 <titans3767@gmail.com>
2023-02-25 15:24:39 -0500

    ASDF

--- src/main/java/frc/robot/commands/Arm/HomeArm.java
+++ src/main/java/frc/robot/commands/Arm/HomeArm.java
@@ -27,6 +27,6 @@ public class HomeArm extends CommandBase{
 
     @Override
     public boolean isFinished() {
-        return arm.reverseLimitSwitch.get();
+        return !arm.reverseLimitSwitch.get();
     }
 }
